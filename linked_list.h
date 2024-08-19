2f09b39b32c118465065e248e33ae8c1a73724b2
Joshua Matthews <jmatth93@gmail.com>
2024-08-18 22:05:10 -0500

    added attributes and proper macros

--- linked_list.h
+++ linked_list.h
@@ -2,10 +2,9 @@
 #define LINKED_LIST_H
 
 #include <stddef.h>
+#include <sys/cdefs.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+__BEGIN_DECLS
 
 struct linked_list {
   struct linked_list *next;
@@ -13,19 +12,17 @@ struct linked_list {
   void* value;
 };
 
-int linked_list_init(struct linked_list *ll);
-int linked_list_insert(struct linked_list *ll, size_t pos, void* value);
-int linked_list_append(struct linked_list *ll, void* value);
-struct linked_list* linked_list_get_pos(struct linked_list *ll, size_t pos);
-struct linked_list* linked_list_get_end(struct linked_list *ll);
-size_t linked_list_get_len(struct linked_list *ll);
-int linked_list_delete(struct linked_list *ll, size_t pos);
-int linked_list_delete_node(struct linked_list *ll);
-void linked_list_free(struct linked_list *ll);
-void linked_list_free_all(struct linked_list *ll);
+int linked_list_init(struct linked_list *ll) __THROWNL __nonnull ((1));
+int linked_list_insert(struct linked_list *ll, size_t pos, void* value) __THROWNL __nonnull ((1,3));
+int linked_list_append(struct linked_list *ll, void* value) __THROWNL __nonnull ((1,2));
+struct linked_list* linked_list_get_pos(struct linked_list *ll, size_t pos) __THROWNL __nonnull ((1));
+struct linked_list* linked_list_get_end(struct linked_list *ll) __THROWNL __nonnull ((1));
+size_t linked_list_get_len(struct linked_list *ll) __THROWNL __nonnull ((1));
+int linked_list_delete(struct linked_list *ll, size_t pos) __THROWNL __nonnull ((1));
+int linked_list_delete_node(struct linked_list *ll) __THROWNL __nonnull ((1));
+void linked_list_free(struct linked_list *ll) __THROWNL __nonnull ((1));
+void linked_list_free_all(struct linked_list *ll) __THROWNL __nonnull ((1));
 
-#ifdef __cplusplus
-}
-#endif
+__END_DECLS
 
 #endif
