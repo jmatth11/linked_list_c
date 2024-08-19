2053d5bd1794cd30ba0801241b205b365db523db
Joshua Matthews <jmatth93@gmail.com>
2024-08-18 21:52:02 -0500

    updated with fixes

--- linked_list.c
+++ linked_list.c
@@ -1,33 +1,63 @@
+#include <stdio.h>
 #include <stdlib.h>
+
 #include "linked_list.h"
 
 int linked_list_init(struct linked_list *ll) {
-  ll->value = NULL;
   ll->next = NULL;
   ll->prev = NULL;
+  ll->value = NULL;
   return 0;
 }
 
-static inline struct linked_list* find_pos(struct linked_list *ll, size_t cur_pos, size_t target_pos) {
+static struct linked_list* find_pos(struct linked_list *ll, size_t target_pos) {
   if (ll == NULL) return NULL;
-  if (cur_pos == target_pos) return ll;
-  return find_pos(ll->next, cur_pos + 1, target_pos);
+  struct linked_list *cur = ll;
+  size_t idx = 0;
+  while (cur != NULL) {
+    if (idx == target_pos) {
+      break;
+    }
+    cur = cur->next;
+    ++idx;
+  }
+  if (idx != target_pos) {
+    return NULL;
+  }
+  return cur;
 }
 
-static inline struct linked_list* find_end(struct linked_list *ll) {
+static struct linked_list* find_end(struct linked_list *ll) {
   if (ll == NULL) return NULL;
-  if (ll->next == NULL) return ll;
-  return find_end(ll);
+  struct linked_list *cur = ll;
+  while (cur->next != NULL) {
+    cur = cur->next;
+  }
+  return cur;
+}
+
+static inline size_t find_len(struct linked_list *ll) {
+  struct linked_list *cur = ll;
+  size_t result = 0;
+  while(cur != NULL) {
+    ++result;
+    cur = cur->next;
+  }
+  return result;
 }
 
 int linked_list_insert(struct linked_list *ll, size_t pos, void* value) {
-  struct linked_list *obj = find_pos(ll, 0, pos);
+  struct linked_list *obj = find_pos(ll, pos);
   if (obj == NULL) return 1;
   struct linked_list *tmp = (struct linked_list*)malloc(sizeof(struct linked_list));
   if (tmp == NULL) return 1;
   linked_list_init(tmp);
   tmp->prev = obj;
   tmp->value = value;
+  if (obj->next != NULL) {
+    obj->next->prev = tmp;
+    tmp->next = obj->next;
+  }
   obj->next = tmp;
   return 0;
 }
@@ -45,20 +75,51 @@ int linked_list_append(struct linked_list *ll, void* value) {
 }
 
 struct linked_list* linked_list_get_pos(struct linked_list *ll, size_t pos) {
-  return find_pos(ll, 0, pos);
+  return find_pos(ll, pos);
 }
 
-int linked_list_delete(struct linked_list *ll, size_t pos, void** out_value) {
-  struct linked_list *obj = find_pos(ll, 0, pos);
+struct linked_list* linked_list_get_end(struct linked_list *ll) {
+  return find_end(ll);
+}
+
+size_t linked_list_get_len(struct linked_list *ll) {
+  return find_len(ll);
+}
+
+int linked_list_delete(struct linked_list *ll, size_t pos) {
+  struct linked_list *obj = find_pos(ll, pos);
   if (obj == NULL) return 1;
   obj->prev->next = obj->next;
   obj->next->prev = obj->prev;
-  *out_value = obj->value;
-  free(obj);
+  linked_list_free(obj);
+  return 0;
+}
+
+int linked_list_delete_node(struct linked_list *ll) {
+  if (ll == NULL) return 1;
+  if (ll->prev != NULL) {
+    ll->prev->next = ll->next;
+  }
+  if (ll->next != NULL) {
+    ll->next->prev = ll->prev;
+  }
+  linked_list_free(ll);
   return 0;
 }
 
 void linked_list_free(struct linked_list *ll) {
-  linked_list_init(ll);
+  ll->next = NULL;
+  ll->prev = NULL;
+  ll->value = NULL;
+  free(ll);
+}
+
+void linked_list_free_all(struct linked_list *ll) {
+  struct linked_list *cur = ll;
+  while(cur != NULL) {
+    struct linked_list* n = cur->next;
+    linked_list_free(cur);
+    cur = n;
+  }
 }
 
--- linked_list.h
+++ linked_list.h
@@ -1,10 +1,10 @@
-#ifndef EDITLITE_LINKED_LIST_H
-#define EDITLITE_LINKED_LIST_H
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
 
 #include <stddef.h>
 
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
 
 struct linked_list {
@@ -17,7 +17,15 @@ int linked_list_init(struct linked_list *ll);
 int linked_list_insert(struct linked_list *ll, size_t pos, void* value);
 int linked_list_append(struct linked_list *ll, void* value);
 struct linked_list* linked_list_get_pos(struct linked_list *ll, size_t pos);
-int linked_list_delete(struct linked_list *ll, size_t pos, void** out_value);
+struct linked_list* linked_list_get_end(struct linked_list *ll);
+size_t linked_list_get_len(struct linked_list *ll);
+int linked_list_delete(struct linked_list *ll, size_t pos);
+int linked_list_delete_node(struct linked_list *ll);
 void linked_list_free(struct linked_list *ll);
+void linked_list_free_all(struct linked_list *ll);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
